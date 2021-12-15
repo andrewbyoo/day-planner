383e3dee2afd535d310057be63401d01ba72a04e
andrewyoo93 <andrew.yoo93@gmail.com>
2021-12-15 17:51:46 -0500

    Added variable for row class and test variable for getting the custom data attribute with row 1, used array from to get the custom data attribute from each element with the row class, currently outputting an error when trying to target the input element under each row

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
 var currentDate = moment().format('dddd, MMMM Do');
-var currentTime = moment().format('h A')
-console.log(currentTime)
+var currentTime = moment().format('H');
 $('#currentDay').text(currentDate);
+
+var rows = $('.row')
+console.log(rows)
+var row1 = rows[0]
+console.log(row1.dataset.hour)
+
+Array.from(rows).forEach(row => {
+  var rowHour = row.dataset.hour;
+console.log(rowHour)
+
+
+// Not working, seems i can't target individual row
+  if(currentTime > rowHour) {
+    row.children(1).css('background-color', '#dddddd')
+  }
+
+  if (currentTime = rowHour) {
+    row.children(1).css('background-color', '#FF7575')
+  }
+})
