722853c9b7de3a21edb1f55fb7e6c2b42efa01b2
andrewyoo93 <andrew.yoo93@gmail.com>
2021-12-15 20:34:40 -0500

    Changed timer displayed on html to a function that updates per second, added a new function to update the color of the time slots per second, and added a reference to the section element sibling to button presses

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,41 @@
-var currentDate = moment().format('dddd, MMMM Do');
-var currentTimeString = moment().format('H');
-var currentTime = parseInt(currentTimeString);
-$('#currentDay').text(currentDate);
+function displayTime() {
+  var currentDate = moment().format('dddd, MMMM Do h:mm:ss A')
+  $('#currentDay').text(currentDate);
+}
+setInterval(displayTime, 1000);
 
-var rows = $('.row')
+function updateColors() {
+  var rows = $('.row')
+  var currentTimeString = moment().format('H');
+  var currentTime = parseInt(currentTimeString);
 
-Array.from(rows).forEach(row => {
-  var rowHour = row.dataset.hour;
-  var rowChildren = row.children
-  var rowInput = rowChildren[1];
+  Array.from(rows).forEach(row => {
+    var rowHour = row.dataset.hour;
+    var rowChildren = row.children
+    var rowInput = rowChildren[1];
 
-  if (currentTime > rowHour) {
-    rowInput.style.backgroundColor = 'rgb(221, 221, 221)';
-  }
+    if (currentTime > rowHour) {
+      rowInput.style.backgroundColor = 'rgb(221, 221, 221)';
+    }
 
-  if (currentTime == rowHour) {
-    rowInput.style.backgroundColor = 'rgb(255, 117, 117)';
-  }
+    if (currentTime == rowHour) {
+      rowInput.style.backgroundColor = 'rgb(255, 117, 117)';
+    }
 
-  if (currentTime < rowHour) {
-    rowInput.style.backgroundColor = 'rgb(127, 255, 123)';
-  }
-})
+    if (currentTime < rowHour) {
+      rowInput.style.backgroundColor = 'rgb(127, 255, 123)';
+    }
+  })
+}
+setInterval(updateColors, 1000);
 
 $('button').click(function(event) {
   event.preventDefault();
   var btnClicked = $(event.target);
+  var plannerTimeSlot = btnClicked.siblings('section');
   var plannerItem = btnClicked.siblings('input');
 
+  console.log(plannerTimeSlot.text())
   console.log(plannerItem.val());
 
   if (!plannerItem.val()) {
